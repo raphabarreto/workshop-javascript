4c22ed362989e425ae8c2385903894df421d12f2
Raphael Barreto <tnt.raphael@gmail.com>
2017-03-17 23:31:17 -0300

    Funções matemáticas

---
+++ funcoes-matematicas/funcoes-matematicas.js
@@ -0,0 +1,15 @@
+var menorNota = Math.min(3, 5, 1, 6, 10);
+console.log('Menor nota', menorNota);
+
+var maiorNota = Math.max(3, 5, 1, 6, 10);
+console.log('Maior nota', maiorNota);
+
+var temperatura = Math.round(22.5);
+console.log('Temperatura', temperatura);
+
+var valorAleatorio = Math.random();
+console.log('Valor aleatorio', valorAleatorio);
+var numero = valorAleatorio * 60;
+console.log('Número', numero);
+var numeroMega = Math.floor(numero) + 1;
+console.log('Número da Mega', numeroMega);
