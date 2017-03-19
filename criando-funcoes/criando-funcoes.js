f64f9dcf276e7ded45576f1abcb4b8733d67ead9
Raphael Barreto <tnt.raphael@gmail.com>
2017-03-18 23:11:28 -0300

    Criando funções

---
+++ criando-funcoes/criando-funcoes.js
@@ -0,0 +1,19 @@
+// function declaration
+function somar(a,b){
+	return a + b;
+}
+
+// function expression
+var subtrair = function (a, b) {
+	return a - b;
+}
+
+var soma = somar(2,3);
+console.log('2 + 3 =', soma);
+
+var sutraido = subtrair(6,2);
+console.log('6 - 2 =', sutraido);
+
+console.log('typeof subtrair', typeof subtrair);
+
+
