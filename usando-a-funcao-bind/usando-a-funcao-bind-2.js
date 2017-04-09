6db94844c111d7594dcc1b5ba0c986bb2ed8cb13
Raphael Barreto <tnt.raphael@gmail.com>
2017-04-09 19:12:07 -0300

    Usando a função bind

---
+++ usando-a-funcao-bind/usando-a-funcao-bind-2.js
@@ -0,0 +1,35 @@
+var Estado = (function() {
+	function Estado(){
+		this.comboEstado = $('#combo-estado');
+	}
+
+	Estado.prototype.iniciar = function(){
+		$.ajax({
+			url: 'http://localhost:8080/estados',
+			method: 'GET',
+			dataType: 'jsonp',
+			success: onEstadosRetornados.bind(this),
+			error: onError
+		});
+	}
+
+	function onEstadosRetornados(estados) {
+		this.comboEstado.html('<option>Selecione o estado</option>');
+		estados.forEach(function(estado) {
+			var optionEstado = $('<option>').val(estado.uf).text(estado.nome);
+			this.comboEstado.append(optionEstado);
+		}.bind(this));
+	}
+
+	function onError() {
+		alert('Erro carregando estados do servidor!');
+	}
+
+	return Estado;
+
+})();
+
+$(function() {
+	var estado = new Estado();
+	estado.iniciar();
+});
